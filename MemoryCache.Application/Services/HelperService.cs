45801c94be1dab8d62b4887af0eba8df76770e9e
Alex Araujo <uareke@hotmail.com>
2024-09-14 18:18:23 -0300

    Init

---
+++ MemoryCache.Application/Services/HelperService.cs
@@ -0,0 +1,17 @@
+﻿using MemoryCache.Application.Services.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryCache.Application.Services;
+
+public  class HelperService: IHelperService
+{
+ 
+}
